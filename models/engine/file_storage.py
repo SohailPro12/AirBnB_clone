82bbdb42700f17e676d70e220d4902fe0d086fa7
Sohail Charef <charefsohayl@gmail.com>
2023-12-08 05:24:08 -0800

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.user import User
 from models.base_model import BaseModel
 import json
 import os
@@ -13,24 +14,34 @@ import os
 
 class FileStorage:
     """
-    A class that serializes instances to a JSON file
-    and deserializes a JSON file to instances
-
-    args:
-        __file_path: string
-        __objects: dictionary
+    FileStorage class for serializing and deserializing instances to/from
+    a JSON file.
     """
-    __file_path = 'file.json'
+    __file_path = "file.json"
     __objects = {}
 
+    classes = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
+            }
+
     def all(self):
-        """returns the dictionary __objects."""
+        """./
+        Returns the dictionary with all objects of a specific class.
+        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id """
-        key = "{}{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
